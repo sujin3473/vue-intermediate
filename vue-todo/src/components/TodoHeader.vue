81633b489e2aab708316afbef9710e8302b6e3ac
sjyim <sjyim@pentaworks.xyz>
2021-02-16 18:33:23 +0900

    02/16 todoList 까지 구현

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1>Todo it!</h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+/* scoped : 컴포넌트 안에서만 유효한 스타일 */
+h1 {
+  color: #2F3852;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
